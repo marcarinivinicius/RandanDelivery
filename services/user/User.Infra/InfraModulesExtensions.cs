947fdc6882103cebc7e988d76e31b82fdeba5221
marcarinivinicius <viniciusantoniomarcarini@gmail.com>
2024-05-13 21:34:41 -0300

    Ajustado pasta do User

---
+++ services/user/User.Infra/InfraModulesExtensions.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RabbitMq.Notify.Interfaces;
+using RabbitMq.Notify.Services;
+using RabbitMq.Notify.Services.RabbitMq.Notify.Services;
+using RabbitMQ.Client;
+using User.Infra.Context;
+using User.Infra.Interfaces;
+using User.Infra.Messages;
+using User.Infra.Repositories;
+
+namespace User.Infra
+{
+    public static class InfraModulesExtensions
+    {
+        public static IServiceCollection AddInfraModules(this IServiceCollection services)
+        {
+            services.AddDatabase();
+            services.AddServiceMessage();
+            return services;
+        }
+
+        public static IServiceCollection AddDatabase(this IServiceCollection services)
+        {
+            services.AddDbContextFactory<UserContext>();
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            return services;
+        }
+
+        public static IServiceCollection AddServiceMessage(this IServiceCollection services)
+        {
+            services.AddSingleton<IRabbitConnection>(sp =>
+            {
+                var configuration = sp.GetRequiredService<IConfiguration>();
+
+                var factory = new ConnectionFactory()
+                {
+                    HostName = configuration["Rabbitmq:HostName"]
+                };
+                factory.AutomaticRecoveryEnabled = true;
+                factory.NetworkRecoveryInterval = TimeSpan.FromSeconds(5);
+                factory.TopologyRecoveryEnabled = true;
+
+                if (!string.IsNullOrWhiteSpace(configuration["Rabbitmq:UserName"]))
+                    factory.UserName = configuration["Rabbitmq:UserName"];
+
+                if (!string.IsNullOrWhiteSpace(configuration["Rabbitmq:Password"]))
+                    factory.Password = configuration["Rabbitmq:Password"];
+                var retryCount = 10;
+
+                if (!string.IsNullOrWhiteSpace(configuration["Rabbitmq:RetryCount"]))
+                    retryCount = int.Parse(configuration["Rabbitmq:RetryCount"]!);
+
+                return new RabbitConnection(factory, retryCount);
+            });
+
+            services.AddScoped<RabbitProducer>();
+            services.AddScoped(typeof(ILoggerAdapter<>), typeof(LoggerAdapter<>));
+            services.AddSingleton<UserRpcListener>();
+            return services;
+        }
+
+    }
+}
---
+++ services/user/User.Infra/Messages/UserRpcListener.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using RabbitMq.Notify.DataModels;
+using RabbitMq.Notify.Interfaces;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using User.Domain.Entities;
+using User.Infra.Context;
+
+namespace User.Infra.Messages
+{
+    public class UserRpcListener
+    {
+
+        private readonly IRabbitConnection _persistentConnection;
+        private readonly IDbContextFactory<UserContext> _context;
+        public UserRpcListener(IRabbitConnection persistentConnection, IServiceScopeFactory factory, IDbContextFactory<UserContext> context)
+        {
+            _persistentConnection = persistentConnection;
+            _context = context;
+        }
+        public void Consume(string queue)
+        {
+            if (!_persistentConnection.IsConnected)
+                _persistentConnection.TryConnect();
+
+            var channel = _persistentConnection.CreateChannel();
+
+            var args = new Dictionary<string, object> { { "x-single-active-consumer", true } };
+
+            channel.QueueDeclare(
+                   queue: queue,
+                   durable: true,
+                   exclusive: false,
+                   autoDelete: false,
+                   arguments: args);
+            channel.BasicQos(0, 1, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume(queue: queue, autoAck: false, consumer: consumer);
+            consumer.Received += async (model, ea) =>
+            {
+                await ReceivedEventAsync(model!, ea, channel);
+            };
+        }
+        private async Task ReceivedEventAsync(object sender, BasicDeliverEventArgs ea, IModel channel)
+        {
+            string response = null;
+
+            var props = ea.BasicProperties;
+            var replyProps = channel.CreateBasicProperties();
+            replyProps.CorrelationId = props.CorrelationId;
+
+            try
+            {
+                var message = Encoding.UTF8.GetString(ea.Body.Span);
+                var data = JsonConvert.DeserializeObject<Request>(message);
+                if (ea.RoutingKey == "publish")
+                {
+                    var dbContext = _context.CreateDbContext();
+                    string method = data!.Method;
+                    List<Client> clients = new List<Client>();
+                    switch (method)
+                    {
+                        case "GetUser":
+                            int userId = data.Payload["Id"];
+                            clients = await dbContext.Clients.AsNoTracking().Where(x => x.Id == userId).ToListAsync();
+                            response = JsonConvert.SerializeObject(new Response() { Success = true, Payload = clients.FirstOrDefault()! });
+                            break;
+                        case "GetUserLogged":
+                            string email = data.Payload["Email"];
+                            clients = await dbContext.Clients.AsNoTracking().Where(x => x.Email == email).ToListAsync();
+                            var client = clients.FirstOrDefault();
+                            if (client != null)
+                            {
+                                // Refatorar o Redis, mover para infra, reestruturar a conexão para buscar aqui se tem autenticação logada para esse usuário; 
+                                // devolver usuário se tiver token autenticado ainda, se não remover.
+                                response = JsonConvert.SerializeObject(new Response() { Success = true, Payload = clients.FirstOrDefault()! });
+                            }
+                            break;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //logging
+                response = JsonConvert.SerializeObject(new Response() { Success = false, ErrMessage = ex.Message });
+            }
+            finally
+            {
+                var responseBytes = Encoding.UTF8.GetBytes(response!);
+                channel.BasicPublish(
+                    exchange: "",
+                    routingKey: props.ReplyTo,
+                    basicProperties: replyProps,
+                    body: responseBytes);
+                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            }
+        }
+
+        public void Disconnect()
+        {
+            _persistentConnection.Dispose();
+        }
+    }
+}
